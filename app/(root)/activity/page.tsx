25e18418b587cb87900da87aa3ed1142ea6c888f
Rodrigo Gedam <rogedam@gmail.com>
2023-08-12 16:26:16 -0300

    Search Page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,16 @@
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 export default async function Page() {
+	const user = await currentUser();
+	if (!user) return null;
+
+	const userInfo = await fetchUser(user.id);
+	if (!userInfo?.onboarded) redirect("/onboarding");
+
+	//getActivity
+
 	return (
 		<section>
 			<h1 className="head-text mb-10">Activity</h1>
