11ca8b6f5f7fb39a6a1e64e10bbfce0849db9027
Rodrigo Gedam <rogedam@gmail.com>
2023-09-04 16:04:10 -0300

    Update auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: "Threads Clone",
 	description: "A Next.Js 13 Meta Threads Clone Application",
 };
@@ -15,12 +17,14 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider
+			appearance={{
+				baseTheme: dark,
+			}}
+		>
 			<html lang="en">
 				<body className={`${inter.className} bg-dark-1`}>
-					<div className="w-full flex justify-center items-center min-h-screen">
-						{children}
-					</div>
+					{children}
 				</body>
 			</html>
 		</ClerkProvider>
